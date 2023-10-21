ba6b0fbd029263f9ca6f084e1e662bebc8227463
Edry Rezal <123053274+masedryer@users.noreply.github.com>
2023-10-21 23:50:21 +0800

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,269 @@
+import img from "../images/CybroSphere.png";
+
+export const Bio = {
+  name: "Edry Rezal",
+  roles: ["Front-End DEveloper", "Software Developer", "Student"],
+  description:
+    "I am a student in Republic Polytechnic pursuing a diploma in digital design and development. Forward-thinking software developer with a passion for web technologies. Strong competence in crafting innovative digital solutions. Committed to continuous self-development and poised to explore future opportunities in e-commerce and business innovation.",
+  github: "https://github.com/rishavchanda",
+  resume:
+    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+  twitter: "https://twitter.com/RishavChanda",
+  insta: "https://www.instagram.com/rishav_chanda/",
+  facebook: "https://www.facebook.com/rishav.chanda.165/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "React",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+    ],
+  },
+  {
+    title: "Backend",
+    skills: [
+      {
+        name: "PHP",
+        image: "https://www.php.net/images/logos/new-php-logo.svg",
+      },
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "MySQL",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+      },
+      {
+        name: "Python",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+      },
+
+      {
+        name: "MongoDB",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+      },
+    ],
+  },
+
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+
+      {
+        name: "Adobe XD",
+        image:
+          "https://camo.githubusercontent.com/c205ecbe12500177d102169d97bc1c17c545155fdf5ec78c08d54ac53e5b38c1/68747470733a2f2f63646e2e776f726c64766563746f726c6f676f2e636f6d2f6c6f676f732f61646f62652d78642e737667",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+        name: "Excel",
+        image: "https://icons8.com/icon/UECmBSgBOvPT/microsoft-excel-2019",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQFQb9_Lb3bZug/company-logo_200_200/0/1644414857251?e=1693440000&v=beta&t=7WqhIiUCg_0otFDwWn-O_SP-Z4Ftoe4be1WlnjYoESc",
+    role: "Frontend Engineer Intern",
+    company: "Neurobit",
+    date: "June 2023 - Present",
+    desc: "Working on the frontend of the web application using ReactJS, Redux, and Material UI.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+    ],
+    doc: "https://firebasestorage.googleapis.com/v0/b/buckoid-917cf.appspot.com/o/Screenshot%20from%202023-05-28%2023-20-46.png?alt=media&token=5570f995-e8f4-4f12-bb2f-73bcf4b20730",
+  },
+  {
+    id: 1,
+    img: "https://media.licdn.com/dms/image/D560BAQFDUKPSU8P0Lw/company-logo_200_200/0/1687947132305?e=1698278400&v=beta&t=cBF3SdszRLspmyvV-rHa1y4tdj_hjjivH7Xy0Z7kOD8",
+    role: "Fullstack Externship",
+    company: "Flipr",
+    date: "June 2023 - July 2023",
+    desc: "Built an employee management full stack web app used Docker and deployed on AWS ec2. I was the top performer in the program.",
+    skills: [
+      "ReactJS",
+      "Redux",
+      "NodeJs",
+      "Material UI",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Docker",
+      "AWS",
+      "MongoDB",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/C560BAQE4UC9Z_k3c7A/company-logo_200_200/0/1579422507409?e=1692230400&v=beta&t=HJssPsrwwkAJAsvFiVIYPdM8GzUuuSK5YlATKgLx3Bc",
+    school: "Kalinga Institute of Industrial Technology, Bhubaneswar",
+    date: "Oct 2021 - Sep 2025",
+    grade: "8.71 CGPA",
+    desc: "I am currently pursuing a Bachelor's degree in Computer Science and Engineering at Kalinga Institute of Industrial Technology, Bhubaneswar. I have completed 4 semesters and have a CGPA of 8.71. I have taken courses in Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, and Computer Networks, among others. I am also a member of the Google Developers Student Club (GDSC) at KIIT, where I am learning and working on exciting projects with a team of talented developers.",
+    degree: "Bachelor of Technology - BTech, Computer Science and Engineering",
+  },
+  {
+    id: 1,
+    img: "https://methodistschool-dankuni.webs.com/NEW%20BADGE.jpg",
+    school: "Methodist School, Dankuni",
+    date: "Apr 2019 - Apr 2025",
+    grade: "88.2%",
+    desc: "I completed my class 12 high school education at Methodist School, Dankuni, where I studied Science with Computer Science.",
+    degree: "ISC(XII), Science with Computer",
+  },
+  {
+    id: 2,
+    img: "https://methodistschool-dankuni.webs.com/NEW%20BADGE.jpg",
+    school: "Methodist School, Dankuni",
+    date: "Apr 2017 - Apr 2019",
+    grade: "92.3%",
+    desc: "I completed my class 10 education at Methodist School, Dankuni, where I studied Science with Computer Application.",
+    degree: "ICSC(X), Science with Computer",
+  },
+];
+
+export const projects = [
+  {
+    id: 9,
+    title: "CybroSphere",
+    date: "Apr 2023 - Jul 2023",
+    description:
+      "Developing a full-stack website application where users can reviews and rate the movies that they had watch.It is also platform for peole to see movies and how good the movie is for audience.Implemented using html,css,bootstrap for front-end and back-end using php and mySQL.In mind of users using small devices, this website is also responsive which make a seamless experience for all devices.   ",
+    image: "https://i.imgur.com/5GT22B7.png",
+    tags: ["PHP", "HTML", "CSS", "BootStrap", "mySQL"],
+    category: "web app",
+    github: "https://github.com/masedryer/MovieReview",
+  },
+  {
+    id: 0,
+    title: "Java Technical Documentation",
+    date: "Aug 2023 - Aug 2023",
+    description:
+      "Developed a front end web application for users to see java concepts such as Object-Oriented Programming.THis documentation provide clwar guide with code snippets and examples.",
+    image: "https://i.imgur.com/VeSCsH7.png",
+    tags: ["Html", "CSS"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Podstream",
+    webapp: "https://podstream.netlify.app/",
+  },
+  {
+    id: 1,
+    title: "CCA Registration Page",
+    date: "Apr 2023 - Jul 2023",
+    description:
+      "Develop a CCA registration page that enables students to apply for CCA and teachers to register new activity and manage applications.This program is created using Java and invloved testing using Junit.This program is created with a team that follows agile methodologies to optimize team efficiency and communcation",
+    image: "https://i.imgur.com/RjbGglj.png",
+    tags: ["Java", "JUnit", "Agile", "Git"],
+    category: "web app",
+    github: "https://github.com/rishavchanda/Project-Management-App",
+    webapp: "https://vexa-app.netlify.app/",
+  },
+  {
+    id: 2,
+    title: "Scoot InFlight entertainment",
+    date: "Jan 2023 - Mar 2023",
+    description:
+      "Develop a mobile wireframe that enables Scoot passengers inflight to be able to buy food & accessories, play games, view flight map and request for service.This app also enables inflight crew to manage the passengers inflight by able to see seat map that can see the overview of the flight and also manage inventory.This is made with team ",
+    image: "https://i.imgur.com/dvIi45i.png",
+    tags: ["Adobe XD"],
+    category: "machine learning",
+    github: "/",
+    webapp: "/",
+    member: [
+      {
+        name: "Rishav Chanda",
+        img: "/",
+        linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+        github: "https://github.com/rishavchanda/",
+      },
+      {
+        name: "Upasana Chaudhuri",
+        img: "/",
+        linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+        github: "https://github.com/upasana0710",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Hospital Management System",
+    date: "Dec 2022 - Mar 2023",
+    description:
+      "Developed a program where hospital staff can manage wards,beds,patients.This program is made purely with Java using fundamentals like Object-Oriented Programming",
+    image: "https://i.imgur.com/ojLSuro.png",
+    tags: ["Java"],
+    category: "android app",
+    github: "/",
+    webapp: "/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
---
+++ src/utils/Themes.js
@@ -0,0 +1,22 @@
+export const darkTheme = {
+  bg: "#27241c",
+  bgLight: "#27241c",
+  primary: "#e6ad4c",
+  text_primary: "#F2F3F4",
+  text_secondary: "#b1b2b3",
+  card: "#211e17",
+  card_light: "#241e19",
+  button: "#854CE6",
+  white: "#FFFFFF",
+  black: "#000000",
+};
+
+export const lightTheme = {
+  bg: "#FFFFFF",
+  bgLight: "#f0f0f0",
+  primary: "#be1adb",
+  text_primary: "#111111",
+  text_secondary: "#48494a",
+  card: "#FFFFFF",
+  button: "#5c5b5b",
+};
